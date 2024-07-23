5a61b514462376a32b18a46baacdc73bccd9c566
mohammed sirjudeen <mohammedsira18@gamil.com>
2024-07-24 00:15:59 +0530

    node file=system

---
+++ README.md
@@ -0,0 +1,9 @@
+# File System using Node js
+
+This project provides a simple file management system using Node.js and Express.js, file system. It includes two main API endpoints for creating a file with the current timestamp and retrieving all files in a specified folder. The folder location is configurable via environment variables. And used moment.js for handling the current date and time stamps.
+
+**All the APIs are well documented using Postman Documentation and Published** - [published API documentation link](https://documenter.getpostman.com/view/34880470/2sA3dxEByh)
+
+
+
+**Note: Since I'm deployed the apis in render's free tier, The initial request is taking time, Please wait little longer for initial request**
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+const folderPath = path.join(__dirname, "Date-Time");
+
+// console.log("__dirname", __dirname);
+// console.log("folderPath", folderPath);
+
+// Check the folder exists
+if (!fs.existsSync(folderPath)) {
+    console.log("Creating public folder");
+    fs.mkdirSync(folderPath);
+}
+
+// Endpoint to create a text file with the current timestamp
+app.post("/create-file", (req, res) => {
+    const timestamp = new Date();
+    const fileName = `${timestamp.toISOString().replace(/:/g, "-")}.txt`;
+    const filePath = path.join(folderPath, fileName);
+
+    fs.writeFile(filePath, timestamp.toString(), (err) => {
+        if (err) {
+            console.log("Error creating file", err);
+            return res.status(500).json({ message: `Error writing file - ${err}` });
+        }
+        res.json({ message: "File created successfully", fileName });
+    });
+});
+
+// Endpoint to retreive all text files in the folder
+app.get("/get-files", (req, res) => {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.log("Error reading files", err);
+            return res.status(500).json({ message: `Error reading files - ${err}` });
+        }
+        res.json({ message: "Files retrieved successfully", files });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
